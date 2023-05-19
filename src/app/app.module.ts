833f5a48a11e97ed3d05b44a5804476e7ea8e060
DenisGabriel017 <denisgabriel017@gmail.com>
2023-05-19 10:44:02 -0300

    Front-end banco de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users/users-list/users-list.component';
 import { UpdateUsersComponent } from './users/update-users/update-users.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
 
 
 @NgModule({
@@ -18,8 +20,10 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
