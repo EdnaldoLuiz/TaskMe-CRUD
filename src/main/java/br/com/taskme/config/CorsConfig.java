6d2e04cc37d665972450ffdeddf74a58088228e6
DenisGabriel017 <denisgabriel017@gmail.com>
2023-05-22 11:01:19 -0300

    conect 1.1

--- src/main/java/br/com/taskme/config/CorsConfig.java
+++ src/main/java/br/com/taskme/config/CorsConfig.java
@@ -1,16 +1,17 @@
 package br.com.taskme.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-    @Configuration
-    public class CorsConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200/users") // Defina as origens permitidas aqui
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-    }
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
