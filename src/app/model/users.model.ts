ccd258a6a4526bf386c22ff14e4bbae8be48c9a3
EdnaldoLuiz <ednaldo.eln68@gmail.com>
2023-05-23 00:35:26 -0300

    updating user interface *finally the spring boot - angular conection workedgit add .add .

--- src/app/model/users.model.ts
+++ src/app/model/users.model.ts
@@ -1,12 +1,22 @@
+import { DatePipe } from '@angular/common';
+
 export class Users {
-    id: number;
-    name: string;
-    email: string;
+  id: number;
+  name: string;
+  email: string;
+  birthDate: string;
+  formattedBirthDate: string;
   
-    constructor(id: number, name: string, email: string) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
-    }
+  constructor(id: number, name: string, email: string, birthDate: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.formattedBirthDate = this.formatBirthDate(birthDate);
   }
   
+  private formatBirthDate(birthDate: string): string {
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(birthDate, 'yyyy-MM-dd') || '';
+  }
+}
