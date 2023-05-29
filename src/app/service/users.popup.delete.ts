20859e46df66e45e00a13fcc175e6a9831adde8f
EdnaldoLuiz <ednaldo.eln68@gmail.com>
2023-05-29 02:23:04 -0300

    updating popup

--- src/app/service/users.popup.delete.ts
+++ src/app/service/users.popup.delete.ts
@@ -4,17 +4,49 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   template: `
     <div class="custom-dialog-content">
-  <h4 mat-dialog-title>Confirmação de exclusão</h4>
-  <div mat-dialog-content>
-    <p>{{ mensagem }}</p>
-  </div>
-  <div mat-dialog-actions>
-    <button mat-button [mat-dialog-close]="true">Sim</button>
-    <button mat-button [mat-dialog-close]="false">Não</button>
-  </div>
-</div>
-
-  `
+    <h4 mat-dialog-title class="dialog-title">
+        <img src="../assets/icons/delete.png" alt="Ícone de exclusão" class="dialog-icon" style="width: 120px">
+      </h4>
+      <!-- <div mat-dialog-content class="dialog-content">
+        <p>{{ mensagem }}</p>
+      </div> -->
+      <div mat-dialog-actions class="dialog-actions">
+        <button mat-raised-button class="delete-button" [mat-dialog-close]="true">Apagar</button>
+        <button mat-raised-button class="cancel-button" [mat-dialog-close]="false">Cancelar</button>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .custom-dialog-content {
+      text-align: center;
+    }
+    
+    .dialog-title {
+      margin-bottom: 20px;
+    }
+    
+    .dialog-content {
+      margin-bottom: 30px;
+    }
+    
+    .dialog-actions {
+      display: flex;
+      justify-content: center;
+    }
+    
+    .delete-button {
+      background-color: red;
+      color: white;
+      margin-right: 10px;
+      width: 70px;
+    }
+    
+    .cancel-button {
+      background-color: green;
+      color: white;
+      width: 70px;
+    }
+  `]
 })
 export class ExclusaoComponente {
   mensagem: string;
