6d3df0a8d048fcc421a21b74f6b005ceeaa5acb5
EdnaldoLuiz <ednaldo.eln68@gmail.com>
2023-06-04 01:13:19 -0300

    updating some css parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { UsersComponent } from './users/users.component';
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
-  // outras rotas do seu aplicativo
 ];
 
 @NgModule({
