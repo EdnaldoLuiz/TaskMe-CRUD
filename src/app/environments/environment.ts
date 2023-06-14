2bba16efb4c5611c73243bb5f631351aa88c4b3d
EdnaldoLuiz <ednaldo.eln68@gmail.com>
2023-06-14 15:14:04 -0300

    fix package tree and gitignore files

--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: 'https://taskme-crud-production.up.railway.app/users'
-  };
-  
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ import { environment } from '../environments/environment';
 export class UsersService {
   private baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>(this.baseUrl);
